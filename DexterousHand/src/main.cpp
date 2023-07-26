323f87358bbd3b7482c83d0be1ef6d9a956c4c74
Yuichiro86 <yuichirobot.s@gmail.com>
2023-07-26 23:56:02 +0900

    2個のセンサーに対応させて、ffとfb制御を切り替えられるようにした。

--- DexterousHand/src/main.cpp
+++ DexterousHand/src/main.cpp
@@ -14,7 +14,8 @@
 
 #define rxPin 16
 #define txPin 17
-
+#define FF 0
+#define FB 1
 
 
 HardwareSerial SerialForServo(PC_11,PC_10);
@@ -22,6 +23,7 @@ EMG_CALIB myemg(Serial2);
 
 SMS_STS SerialServo;
 Adafruit_BNO055 IMU_arm(55,0x28);//IMUを扱うインスタンスを生成
+Adafruit_BNO055 IMU_hand(56,0x29);//IMUを扱うインスタンスを生成
 Serial_debug debug(Serial2);
 CountTimer Ctimer;
 
@@ -37,6 +39,7 @@ int posZ = 90;
 int servo_v = 3600;
 int servo_a = 80;
 int mode = 0;// 0:開＋OFF　１：閉＋OFF　２閉＋ON
+int control = 0;//
 unsigned long wait_time = 0;
 
 bool LED = true;
@@ -67,6 +70,7 @@ void setup(){
   
   //bool hand_conection = IMU_hand.begin();//begin()でIMUとの通信開始
   bool arm_conection = IMU_arm.begin();
+  bool hand_conection = IMU_hand.begin();
 
   if(!arm_conection){
     Serial2.println("no BNO055(arm) detected");
@@ -76,21 +80,43 @@ void setup(){
     Serial2.println("IMU_arm setting complete");
   }
 
+  if(!hand_conection){
+    Serial2.println("no BNO055(hand) detected");
+    while(1){}
+  }else{
+    IMU_hand.setExtCrystalUse(true);
+    Serial2.println("IMU_hand setting complete");
+  }
+
   IMU_arm.setAxisRemap(Adafruit_BNO055::REMAP_CONFIG_P2);
   IMU_arm.setAxisSign(Adafruit_BNO055::REMAP_SIGN_P2);
   
-  uint8_t mag_calib;
-  uint8_t gyro_calib;
-  uint8_t sys_calib;
-  uint8_t accel_calib;
-  
+
+  IMU_hand.setAxisRemap(Adafruit_BNO055::REMAP_CONFIG_P2);
+  IMU_hand.setAxisSign(Adafruit_BNO055::REMAP_SIGN_P2);
+
+
+  uint8_t a_mag_calib=0;
+  uint8_t a_gyro_calib=0;
+  uint8_t a_sys_calib=0;
+  uint8_t a_accel_calib=0;
+
+  uint8_t h_mag_calib=0;
+  uint8_t h_gyro_calib=0;
+  uint8_t h_sys_calib=0;
+  uint8_t h_accel_calib=0;
+
   do
   {
-    Serial2.print("calibrating.........");
-    Serial2.print(mag_calib);
+    Serial2.print("IMU_arm calibrating...");
+    Serial2.print(a_mag_calib);
+    Serial2.print("of3   ");
+    Serial2.print("IMU_hand calibrating...");
+    Serial2.print(h_mag_calib);
     Serial2.println("of3");
-    IMU_arm.getCalibration(&sys_calib,&gyro_calib,&accel_calib,&mag_calib);
-  } while (mag_calib != 3);
+    IMU_arm.getCalibration(&a_sys_calib,&a_gyro_calib,&a_accel_calib,&a_mag_calib);
+    IMU_arm.getCalibration(&h_sys_calib,&h_gyro_calib,&h_accel_calib,&h_mag_calib);
+  } while (a_mag_calib != 3 || h_mag_calib != 3);
 
   while (!SerialForServo) delay(1);
 
@@ -128,6 +154,12 @@ void loop(){
     result = judge_1to2 + 0.1;
   }else if(SoftSerial_str == "0"){
     result = 0;
+  }else if(SoftSerial_str == "ff"){
+    control = FF;
+    result = judge_1to2 + 0.1;
+  }else if(SoftSerial_str == "fb"){
+    control = FB;
+    result = judge_1to2 + 0.1;
   }
 
   switch (mode)
@@ -138,7 +170,7 @@ void loop(){
     SerialServo.RegWriteAction();
     move_time(2600,2047,servo_v,servo_a);
     
-
+  
     if(judge_0to1 <= result){//閾値より高かったらmode変更
     mode = 1;
     }
@@ -146,8 +178,8 @@ void loop(){
   break;}
 
   case 1/*義手を閉じて水平制御OFF*/:{
-    SerialServo.RegWritePosEx(4,2600,servo_v,servo_a);//指を閉じる
-    SerialServo.RegWritePosEx(5,1400,servo_v,servo_a);
+    SerialServo.RegWritePosEx(4,2300,servo_v,servo_a);//指を閉じる
+    SerialServo.RegWritePosEx(5,1600,servo_v,servo_a);
     SerialServo.RegWriteAction();
     move_time(2600,2047,servo_v,servo_a);
     
@@ -165,8 +197,6 @@ void loop(){
     digitalWrite(13,LED);
     LED = !LED;
 
-    if(Ctimer.wait(&wait_time)){
-
       posX = StepRange_con(SerialServo.ReadPos(1));//-2027 < (step) < 2027
       posZ = StepRange_con(SerialServo.ReadPos(2));
       posY = StepRange_con(SerialServo.ReadPos(3));
@@ -176,62 +206,73 @@ void loop(){
       imu::Quaternion qua_now = IMU_arm.getQuat();//現在のクォータニオンを取得
       
       debug.TimerDebug("GetQua");
+      //Common processing
+      
+      if(Ctimer.wait(&wait_time)){
+      
+        switch (control){
+        case FF:{
+          imu::Quaternion diff = diffQuaterniopn(qua_aim,qua_now);
 
-      imu::Quaternion diff = diffQuaterniopn(qua_aim,qua_now);
+          debug.TimerDebug("GetDiff");
 
-      debug.TimerDebug("GetDiff");
+          imu::Vector<3> vec = convertEuler(diff,EulerOrder::XZY);//x-z-y 0 < (rad) < 2pi
 
-      imu::Vector<3> vec = convertEuler(diff,EulerOrder::XZY);//x-z-y 0 < (rad) < 2pi
+          debug.TimerDebug("Convert");
+          debug.WebSerialprint((float)diff.w(),(float)diff.x(),(float)diff.y(),(float)diff.z(),3,3,3,3);
 
-      debug.TimerDebug("Convert");
-      //debug.WebSerialprint((float)diff.w(),(float)diff.x(),(float)diff.y(),(float)diff.z(),3,3,3,3);
+          vec.x() = RadRange_con(vec.x());// -pi < rad < pi
+          vec.z() = RadRange_con(vec.z());
+          vec.y() = RadRange_con(vec.y());
 
-      vec.x() = RadRange_con(vec.x());// -pi < rad < pi
-      vec.z() = RadRange_con(vec.z());
-      vec.y() = RadRange_con(vec.y());
+          int dx = RadToStep(vec.x());//オイラー角をサーボのステップ数に変換
+          int dz = RadToStep(vec.z());//これ差分になる絶対角度で、センサーが根元にしかついていないから先端が水平になっていても小さくならない。モーターの目標角度になる。
+          int dy = RadToStep(vec.y());//-2047 < (step) < 2047
 
-      int dx = RadToStep(vec.x());//オイラー角をサーボのステップ数に変換
-      int dz = RadToStep(vec.z());//これ差分になる絶対角度で、センサーが根元にしかついていないから先端が水平になっていても小さくならない。モーターの目標角度になる。
-      int dy = RadToStep(vec.y());//-2047 < (step) < 2047
+          
+
+          dx = constrain(StepRange_res(dx),1000,3000);
+          dz = constrain(StepRange_res(dz),1500,2500);
+          dy = constrain(StepRange_res(dy),1500,2700);// 0 < step < 4054
+          
+        
+          Serial2.print("dx:");
+          Serial2.print(dx);
+          Serial2.print(" ");
+          Serial2.print("dz:");
+          Serial2.print(dz);
+          Serial2.print(" ");
+          Serial2.print("dy:");
+          Serial2.print(dy);
+          Serial2.print(" ");
+
+          debug.TimerDebug("getMovement");
+
+          //MsTimer2::stop();//割り込み止める2
+          SerialServo.RegWritePosEx(1,dx,servo_v,servo_a);//手首サーボを駆動
+          SerialServo.RegWritePosEx(2,4054-dz,servo_v,servo_a);//絶対角度指定
+          SerialServo.RegWritePosEx(3,4054-dy,servo_v,servo_a);
+          /*
+          SerialServo.RegWritePosEx(4,2600,servo_v,servo_a);//指を閉じる
+          SerialServo.RegWritePosEx(5,1400,servo_v,servo_a);
+          */
+        
+          SerialServo.RegWriteAction();
+
+          debug.TimerDebug("Send");
+          //wait_time = max( move_time(dx,posX,servo_v,servo_a), max(move_time(dz,posZ,servo_v,servo_a), move_time(dy,posY,servo_v,servo_a) ) );//サーボを動作時間待機
+          wait_time = 0;
+          Serial2.print("wait_time:");
+          Serial2.print(wait_time);
+          Serial2.print(" ");
+
+          break;}
+        
+        default:
+          break;
+        }
 
       
-
-      dx = constrain(StepRange_res(dx),1000,3000);
-      dz = constrain(StepRange_res(dz),1500,2500);
-      dy = constrain(StepRange_res(dy),1500,2700);// 0 < step < 4054
-      
-    
-      Serial2.print("dx:");
-      Serial2.print(dx);
-      Serial2.print(" ");
-      Serial2.print("dz:");
-      Serial2.print(dz);
-      Serial2.print(" ");
-      Serial2.print("dy:");
-      Serial2.print(dy);
-      Serial2.print(" ");
-
-      debug.TimerDebug("getMovement");
-
-      //MsTimer2::stop();//割り込み止める2
-      SerialServo.RegWritePosEx(1,dx,servo_v,servo_a);//手首サーボを駆動
-      SerialServo.RegWritePosEx(2,4054-dz,servo_v,servo_a);//絶対角度指定
-      SerialServo.RegWritePosEx(3,4054-dy,servo_v,servo_a);
-      /*
-      SerialServo.RegWritePosEx(4,2600,servo_v,servo_a);//指を閉じる
-      SerialServo.RegWritePosEx(5,1400,servo_v,servo_a);
-      */
-     
-      SerialServo.RegWriteAction();
-
-      debug.TimerDebug("Send");
-      //wait_time = max( move_time(dx,posX,servo_v,servo_a), max(move_time(dz,posZ,servo_v,servo_a), move_time(dy,posY,servo_v,servo_a) ) );//サーボを動作時間待機
-
-      Serial2.print("wait_time:");
-      Serial2.print(wait_time);
-      Serial2.print(" ");
-
-      wait_time = 0;
       Ctimer.wait(&wait_time);
     }
 
@@ -246,6 +287,13 @@ void loop(){
   }
   Serial2.println("");
 }
+
+
+
+
+
+
+
 
 void Interrupt(){
   if(counter>=loop_count){//測定値を積分か平均をとる
