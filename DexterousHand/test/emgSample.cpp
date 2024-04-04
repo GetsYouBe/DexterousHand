77757e995638de2e3e48d5ed915bcecc21593d5e
yuichiro <yuichirobot.s@gmail.com>
2024-04-04 22:22:00 +0900

    add samplecodes

--- DexterousHand/test/emgSample.cpp
+++ DexterousHand/test/emgSample.cpp
@@ -1,78 +1,48 @@
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>//IMUのライブラリ
-
 #include <Arduino.h>
+#include "emg_calib.h"
 
-#include "calculate.h"
-
-Adafruit_BNO055 IMU(55,0x29);//IMUを扱うインスタンスを生成
-
-EulerOrder axisOrder = EulerOrder::XYZ;//関節の座標系指定
-
-double aim_qua_w=1;//目標クォータニオンの各要素
-double aim_qua_x=0;
-double aim_qua_y=0;
-double aim_qua_z=0;
-imu::Quaternion qua_aim(aim_qua_w,aim_qua_x,aim_qua_y,aim_qua_z);//目標クォータニオン生成
+EMG_CALIB emg_1(Serial,1);//Serial
 
 void setup(){
+    Serial.begin(115200);
+    while(!Serial) delay(100);//シリアル通信の確立まで待機（Arduino純正ボード以外では必須)
+}
+void loop(){
+    code1();
 
-    Serial2.begin(115200);
-
-    while(!Serial2) delay(100);//シリアル通信の確立まで待機（Arduino純正ボード以外では必須）
-
-    bool imu_conection = IMU.begin();//IMUとの接続を取得
-    if(!imu_conection){
-        Serial2.println("no BNO055 detected");//接続に失敗していたら待機
-        while(1){}
-    }else{
-        IMU.setExtCrystalUse(true);//IMU内部の水晶振動子を使うかどうか（このコピペでいい）
-        Serial2.println("IMU_arm setting complete");//接続に成功
-    }
-
-
-        uint8_t mag_calib;//IMUのキャリブレーションの数値格納用
-        uint8_t gyro_calib;
-        uint8_t sys_calib;
-        uint8_t accel_calib;
-        
-        do//IMUのコンパスの初期化（これを待たずに制御始めると途中で角度が大きく変動して暴走）
-        {   //ジャイロ、加速度、コンパスのキャリブレーションの状態（１が最低３が最高）を取得
-            IMU.getCalibration(&sys_calib,&gyro_calib,&accel_calib,&mag_calib);
-            Serial2.print("calibrating.........");
-            Serial2.print(mag_calib);
-            Serial2.println("of3");
-        } while (mag_calib != 3);//コンパスのキャリブレーションが最高になるまでループ回して待機
-
-        IMU.setAxisRemap(Adafruit_BNO055::REMAP_CONFIG_P3);//IMUの軸、座標設定
-        IMU.setAxisSign(Adafruit_BNO055::REMAP_SIGN_P3);//これについてはBNO055のデーターシート読んでほしい
+    
 }
 
-void loop(){
+void code1(){
+    float sample;
+    bool result;
+    
+    //sampleに測定結果を格納
+    emg_1.sampling(sample);
+    Serial.println(sample);
 
-    //シリアル通信の確立まで待機（Arduino純正ボード以外では必須）
-    while(!Serial2) delay(100);
+    //resultに判定結果を格納
+    //threshold_calibを1回も実行してないときは、筋電位センサの信号が
+    //2.5vを超えているかで判定される
+    emg_1.judge(result);
+    Serial.println(result);
 
-    //BNO055からクォータニオンで姿勢を取得
-    imu::Quaternion qua_measured = IMU.getQuat();
+    delay(500);
+    
+}
 
-    //qua_aimに対するqua_measuredの差分を求める
-    imu::Quaternion qua_diff = diffQuaterniopn(qua_aim, qua_measured);
+void code2(){
+    float sample,x;
+    bool result;
 
-    //qua_diffをXYZの軸構成のオイラー角に変換する
-    imu::Vector<3> euler = convertEuler(qua_diff,EulerOrder::XYZ);
+    //最大値の7割を閾値として、生成された閾値をxに格納
+    emg_1.threshold_calib(x,7);
+    delay(1000);
 
+    //設定した閾値で判定
+    emg_1.judge(result);
+    Serial.println(result);
+    delay(1000);
 
-    //オイラー角の各要素を出力
-    //実用的にはモーターの角度に指定する
-    //オイラー角の軸構成によらずに.x()のメゾットはｘ要素、.y()のメゾットはy要素という風にアクセスできる
-    Serial2.print("X: ");
-    Serial2.print(euler.x());
-
-    Serial2.print("Y: ");
-    Serial2.print(euler.y());
-
-    Serial2.print("Z: ");
-    Serial2.println(euler.z());
+    
 }
---
+++ DexterousHand/test/imuSample.cpp
@@ -0,0 +1,78 @@
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>//IMUのライブラリ
+
+#include <Arduino.h>
+
+#include "calculate.h"
+
+Adafruit_BNO055 IMU(55,0x29);//IMUを扱うインスタンスを生成
+
+EulerOrder axisOrder = EulerOrder::XYZ;//関節の座標系指定
+
+double aim_qua_w=1;//目標クォータニオンの各要素
+double aim_qua_x=0;
+double aim_qua_y=0;
+double aim_qua_z=0;
+imu::Quaternion qua_aim(aim_qua_w,aim_qua_x,aim_qua_y,aim_qua_z);//目標クォータニオン生成
+
+void setup(){
+
+    Serial.begin(115200);
+
+    while(!Serial) delay(100);//シリアル通信の確立まで待機（Arduino純正ボード以外では必須）
+
+    bool imu_conection = IMU.begin();//IMUとの接続を取得
+    if(!imu_conection){
+        Serial.println("no BNO055 detected");//接続に失敗していたら待機
+        while(1){}
+    }else{
+        IMU.setExtCrystalUse(true);//IMU内部の水晶振動子を使うかどうか（このコピペでいい）
+        Serial.println("IMU_arm setting complete");//接続に成功
+    }
+
+
+        uint8_t mag_calib;//IMUのキャリブレーションの数値格納用
+        uint8_t gyro_calib;
+        uint8_t sys_calib;
+        uint8_t accel_calib;
+        
+        do//IMUのコンパスの初期化（これを待たずに制御始めると途中で角度が大きく変動して暴走）
+        {   //ジャイロ、加速度、コンパスのキャリブレーションの状態（１が最低３が最高）を取得
+            IMU.getCalibration(&sys_calib,&gyro_calib,&accel_calib,&mag_calib);
+            Serial.print("calibrating.........");
+            Serial.print(mag_calib);
+            Serial.println("of3");
+        } while (mag_calib != 3);//コンパスのキャリブレーションが最高になるまでループ回して待機
+
+        IMU.setAxisRemap(Adafruit_BNO055::REMAP_CONFIG_P3);//IMUの軸、座標設定
+        IMU.setAxisSign(Adafruit_BNO055::REMAP_SIGN_P3);//これについてはBNO055のデーターシート読んでほしい
+}
+
+void loop(){
+
+    //シリアル通信の確立まで待機（Arduino純正ボード以外では必須）
+    while(!Serial) delay(100);
+
+    //BNO055からクォータニオンで姿勢を取得
+    imu::Quaternion qua_measured = IMU.getQuat();
+
+    //qua_aimに対するqua_measuredの差分を求める
+    imu::Quaternion qua_diff = diffQuaterniopn(qua_aim, qua_measured);
+
+    //qua_diffをXYZの軸構成のオイラー角に変換する
+    imu::Vector<3> euler = convertEuler(qua_diff,EulerOrder::XYZ);
+
+
+    //オイラー角の各要素を出力
+    //実用的にはモーターの角度に指定する
+    //オイラー角の軸構成によらずに.x()のメゾットはｘ要素、.y()のメゾットはy要素という風にアクセスできる
+    Serial.print("X: ");
+    Serial.print(euler.x());
+
+    Serial.print("Y: ");
+    Serial.print(euler.y());
+
+    Serial.print("Z: ");
+    Serial.println(euler.z());
+}
